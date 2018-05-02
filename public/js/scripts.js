5e8b81ae017a724e6a5e5d42515c6ec586889996
Steven Lawson <unluckygamblermusic@gmail.com>
2018-05-02 17:55:06 -0600

    fetch and post to projects added, dynamically appearing in options dropdown

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,4 @@
+
 let randomColor = Math.floor(Math.random()*16777215).toString(16);
 let colorArray = [];
 
@@ -6,10 +7,60 @@ const newColor = () => {
 }
 
 const elements = ['.one','.two', '.three', '.four', '.five']
+
 let palettes = [];
+let projects = [];
+
+const loopedProjects = projects.forEach(project => console.log(project));
+
+const populateProjectNames = (projectNames) => {
+  $.each(projectNames, (index, project) => {
+    $('.select-options').append($(`<option>`, {
+      value: project.id,
+      text: project.title
+    }))
+  })
+}
+
+const getProjectNames = async () => {
+  try {
+    const response = await fetch('http://localhost:3000/api/v1/projects');
+    const projectNames = await response.json();
+    populateProjectNames(projectNames)
+    console.log('innerprojects',projects)
+  } catch(error) {
+    throw new Error('unable to get projects' + error)
+  }
+}
+
+getProjectNames();
+
+const postProject = async (projects) => {
+  try {
+    const response = await fetch('http://localhost:3000/api/v1/projects', {
+      method: 'POST',
+      body: JSON.stringify(projects),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  } catch(error) {
+    throw new Error('unable to post project' + error)
+  }
+}
+
+const objMaker = (string) => {
+  return { title: string }
+}
+
+const addProject = (event) => {
+  const project = $('.new-project').val();
+  const projObj = objMaker(project)
+  postProject(projObj)
+}
 
 const addPalette = (event) => {
-  console.log(event.target)
+
 }
 
 const changeColor = () => {
@@ -21,11 +72,12 @@ const changeColor = () => {
 }
 
 const lockColor = () => {
-  console.log(event.target.parentNode)
   $(event.target.parentNode).toggleClass('locked')
   $(event.target).toggleClass('locked-btn')
 }
 
+
+$('.project-btn').on('click', addProject)
 $('.palette-btn').on('click', addPalette)
 $('.save').on('click', lockColor)
 $('.generate-btn').on('click', changeColor);
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
@@ -12,12 +16,21 @@ app.get('/', (request, response) => {
 })
 
 app.post('/api/v1/projects', (request, response) => {
-
+  const project = request.body;
+  database('projects').insert(project, 'id')
+  .then(project => {
+    response.status(201).json({ id: project[0] })
+  })
+  .catch(error => {
+    response.status(500).json({ error })
+  })
 })
-
+    
 app.get('/api/v1/projects', (request, response) => {
-  
-})
+  database('projects').select()
+  .then(projects => response.status(200).json(projects))
+  .catch(error => response.status(500).json(error))
+});
 
 app.listen(app.get('port'), () => {
   console.log('Palette Picker be a listenin');
